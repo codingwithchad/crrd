680dda0ecbbfbb7cc7116f8c8ad3eff0f435542f
chadg1980 <chadg1980@gmail.com>
2015-04-30 17:07:42 -0700

    fixed some errors
    This is just the start!

--- core/coreAPP/models.py
+++ core/coreAPP/models.py
@@ -1,19 +1,19 @@
 import datetime
 from django.db import models
-import django.utils import timezone
+from django.utils import timezone
 
 class ReuseCategories(models.Model):
     name = models.CharField(max_length=255)
     lastUpdate = models.DateTimeField('date updated')
     def __str__(self):
-        return sel.ReuseCategories
+        return self.name
 
 class ReuseItem(models.Model):
     itemCategory = models.ForeignKey(ReuseCategories)
     itemName = models.CharField(max_length=255)
     lastUpdate = models.DateTimeField('last updated')
     def __str__(self):
-        return self.ReuseItem
+        return self.itemName
 
 
 
