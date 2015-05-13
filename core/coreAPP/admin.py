f38782262a7a5bb59f49a795c19b73154a0323af
Chad H <glaserc@onid.oregonstate.edu>
2015-05-12 19:07:58 -0800

    Got the admin page up and running
    I added 1 category (large-applianes)
    I have not added access to all the tables, but I am working on that. The
    admin page is coming along.

--- core/coreAPP/admin.py
+++ core/coreAPP/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 from .models import UseCat, UseItems
 
+
+class ItemsInline(admin.TabularInline):
+	model = UseItems
+	extra = 4
+
 class UseCatAdmin(admin.ModelAdmin):
 	fieldset = [
 	(None, {'fields': ['catName']}),
 	('Date information', {'fields':['lastUpdate']}),
 	]
-
+	inlines = [ItemsInline]
+	list_display = ('catName', 'lastUpdate')
+	
+	
+	
+	
 # Register your models here.
 admin.site.register(UseCat, UseCatAdmin)
-admin.site.register(UseItems)
+
 
