764ff88388dac89fb2fc3621c70f906dca7286ce
Justin Hammar <justin.hammar@gmail.com>
2015-05-13 16:40:12 -0500

    made optional fields optional

--- core/coreAPP/models.py
+++ core/coreAPP/models.py
@@ -13,7 +13,7 @@ from django.utils import timezone
 class UseCat(models.Model):
     catName = models.CharField(max_length=200)
 #    adminID = models.ForeignKey(Admin)
-    lastUpdate = models.DateTimeField('date updated')
+    lastUpdate = models.DateTimeField('date updated', blank=True)
     def __str__(self):
         return self.catName
 
@@ -21,7 +21,7 @@ class UseItems(models.Model):
     itemCat = models.ForeignKey(UseCat)
     itemName = models.CharField(max_length=200)
 #    adminID = models.ForeignKey(Admin)
-    lastUpdate = models.DateTimeField('last updated')
+    lastUpdate = models.DateTimeField('last updated', blank=True)
     def __str__(self):
         return self.itemName
 
@@ -32,19 +32,19 @@ class BusItem(models.Model):
 class Business(models.Model):
     busName = models.CharField(max_length=200)
     repairBus = models.NullBooleanField()
-    address1 = models.CharField(max_length=200)
-    address2 =  models.CharField(max_length=200, blank=True, null=True)
-    city =  models.CharField(max_length=200)
-    state =  models.CharField(max_length=200)
-    zipcode =  models.IntegerField()
-    phone1 =  models.CharField(max_length=200)
-    phone2 =  models.CharField(max_length=200)
-    email = models.EmailField()
-    web =  models.URLField(max_length=200)
-    geolocal =  models.CharField(max_length=200)
-    hours =  models.CharField(max_length=200)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 =  models.CharField(max_length=200, blank=True)
+    city =  models.CharField(max_length=200, blank=True)
+    state =  models.CharField(max_length=200, blank=True)
+    zipcode =  models.IntegerField(blank=True, null=True)
+    phone1 =  models.CharField(max_length=200, blank=True)
+    phone2 =  models.CharField(max_length=200, blank=True)
+    email = models.EmailField(blank=True)
+    web =  models.URLField(max_length=200, blank=True)
+    geolocal =  models.CharField(max_length=200, blank=True)
+    hours =  models.CharField(max_length=200, blank=True)
 #    adminID = models.ForeignKey(Admin)
-    lastUpdate = models.DateTimeField('last updated')
+    lastUpdate = models.DateTimeField('last updated', blank=True, null=True)
     def __str__(self):
         return self.busName
 
