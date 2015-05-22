b19cbda1f8f4884a35354250ca22cb54a2f90cd2
Chad H <glaserc@onid.oregonstate.edu>
2015-05-21 17:57:22 -0800

    Changes
    busName, ItemName, and CatName must be unique, Added some humad readable
    names. Created RepItem and RepCat for repair items and repair
    categories. Added Business Repair Items. Needs work before it will
    commit. I am working on getting the many to many Business to Item
    relationship in line for easy editing.

--- core/coreAPP/admin.py
+++ core/coreAPP/admin.py
@@ -1,26 +1,50 @@
 from django.contrib import admin
-from .models import UseCat, UseItems, BusItem, Business, RepCat
+from .models import UseCat, UseItem, BusItem, Business, RepCat, RepItem, BusRepItem
 
-
-class ItemsInline(admin.TabularInline):
-	model = UseItems
+#Allows 4 text boxes to be displayed by default while adding items in Use Cat
+#more can be inserted within the browser
+class UseItemInline(admin.TabularInline):
+	model = UseItem
 	extra = 4
-
+#This allows the UseCat for the admin page
+#Category Name is on top and 4 text boxes for Item names are underneath
+#more text boxes can be added on the fly
 class UseCatAdmin(admin.ModelAdmin):
 	fieldset = [
 	(None, {'fields': ['catName']}),
 	('Date information', {'fields':['lastUpdate']}),
 	]
-	inlines = [ItemsInline]
+	inlines = [UseItemInline]
 	list_display = ('catName', 'lastUpdate')
 	
+class RepItemInline(admin.TabularInline):
+	model = RepItem
+	extra = 4
+
+class RepCatAdmin(admin.ModelAdmin):
+	fieldset = [
+	(None, {'fields': ['repName']}),
+	('Date information', {'fields':['lastUpdate']}),
+	]
+	inlines = [RepItemInline]
+	list_display = ('repName', 'lastUpdate')
+
+class BusinessToItemsInline(admin.TabularInline):
+	model = UseItem
+
+class BusItemAdmin(admin.ModelAdmin):
+	inlines=[BusinessToItemsInline,]
+	
+
 	
 	
 	
 # Register your models here.
 admin.site.register(UseCat, UseCatAdmin)
-admin.site.register(BusItem)
 admin.site.register(Business)
-admin.site.register(RepCat)
-
+#admin.site.register(UseItem)
+admin.site.register(RepCat, RepCatAdmin)
+admin.site.register(RepItem)
+admin.site.register(BusItem)
+admin.site.register(UseItem, BusItemAdmin)
 
--- core/coreAPP/models.py
+++ core/coreAPP/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 #class Admin(models.Model):
@@ -11,28 +12,24 @@ from django.utils import timezone
 #        return self.userName
 
 class UseCat(models.Model):
-    catName = models.CharField(max_length=200)
+    catName = models.CharField('Category Name', max_length=200, unique=True)
 #    adminID = models.ForeignKey(Admin)
     lastUpdate = models.DateTimeField('date updated', auto_now=True, blank=True)
     def __str__(self):
         return self.catName
 
-class UseItems(models.Model):
+class UseItem(models.Model):
     itemCat = models.ForeignKey(UseCat)
-    itemName = models.CharField(max_length=200)
+    itemName = models.CharField('item name', max_length=200, unique=True)
 #    adminID = models.ForeignKey(Admin)
     lastUpdate = models.DateTimeField('last updated', auto_now=True, blank=True)
     def __str__(self):
         return self.itemName
 
-class BusItem(models.Model):
-    businesses = models.ForeignKey('coreAPP.Business')
-    items = models.ForeignKey(UseItems)
-    def __str__(self):
-        return self.businesses.busName + "-" + self.items.itemName
+
 
 class Business(models.Model):
-    busName = models.CharField(max_length=200)
+    busName = models.CharField('Business Name', max_length=200, unique=True)
     repairBus = models.NullBooleanField()
     address1 = models.CharField(max_length=200, blank=True)
     address2 =  models.CharField(max_length=200, blank=True)
@@ -50,11 +47,36 @@ class Business(models.Model):
     def __str__(self):
         return self.busName
 
+
+
+class BusItem(models.Model):
+    businesses = models.ForeignKey(Business)
+    items = models.ForeignKey(UseItem)
+    busItemObject = GenericForeignKey("content_type", "object_id")
+    lastUpdate = models.DateTimeField('last updated', auto_now=True, blank=True)
+    def __str__(self):
+        return self.businesses.busName + "-" + self.items.itemName
+
 class RepCat(models.Model):
-    repName = models.CharField(max_length=200)
+    repName = models.CharField(max_length=200, unique=True)
 #    adminID = models.ForeignKey(Admin)
+    lastUpdate = models.DateTimeField('last update', auto_now=True, blank=True)
     def __str__(self):
         return self.repName
+
+class RepItem(models.Model):
+    RepCat = models.ForeignKey(RepCat)
+    RepItemName = models.CharField(max_length=200, unique=True)
+    lastUpdate = models.DateTimeField('last update', auto_now=True, blank=True)
+    def __str__(self):
+        return self.RepItemName
+
+class BusRepItem(models.Model):
+    business = models.ForeignKey('coreAPP.Business')
+    repItem = models.ForeignKey(RepItem)
+    lastUpdate = models.DateTimeField('last update', auto_now=True, blank=True)
+    def __str__(self):
+        return self.business.busName +"-"+ self.repItem.RepItemName
 
 
 
