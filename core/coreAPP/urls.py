1545ace6c663ab736b261e98c672604780f6d89e
Chad H <glaserc@onid.oregonstate.edu>
2015-06-10 20:23:19 -0700

    Last update
    Seperated business into two sections, added a one to many relationship,
    added URLs for repair items and got the business\items to work.

--- core/coreAPP/urls.py
+++ core/coreAPP/urls.py
@@ -4,8 +4,13 @@ from . import views
 urlpatterns = [
 
     url(r'^$', views.index, name='index'),
-    url(r'^usecat/$', views.viewUseCat, name='categories'),
     
+    url(r'^usecat/$', views.viewUseCat, name='categories'),
     url(r'^(?P<catID>[0-9]+)/useitems/$', views.viewUseItems, name='items'),
-    url(r'^(?P<itemID>[0-9]+)/business/$', views.viewBusiness, name='business'),
+    url(r'^(?P<itemID>[0-9]+)/business/$', views.viewBusiness, name='businesses'),
+
+    url(r'^repcat/repair/$', views.viewRepCat, name='repair categories'),
+    url(r'^(?P<repCatID>[0-9]+)/repitems/$', views.viewRepItems, name='repair items'),
+    url(r'^(?P<repItemID>[0-9]+)/repbusiness/$', views.viewRepairBusiness, name='repair businesses'),
+
     ]
